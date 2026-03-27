95fb2dfca5cf31a6c63e47c81a6b178fd1b35972
Aditya <aditya0dagar@gmail.com>
2026-03-27 18:30:49 +0530

    understiod variables understanding datatypes

---
+++ vars.js
@@ -0,0 +1,45 @@
+// varibales 
+var num1 = 34;
+var num2 = 35;
+console.log(num1 + num2)
+
+// Data types in js 
+// strings
+var st1 = "This is a string in dq"
+var st2 = 'This is a string in sq'
+// num
+var num1 = 34;
+var num2 = 35;
+
+var marks = {
+    ravi: 34,
+    mavi: 35,
+    savi: 56,
+}
+
+console.log(marks);
+console.log(marks["ravi"]);
+console.log(marks["mavi"]);
+console.log(marks["savi"]);
+
+// Booleans
+var a = true;
+var b = false;
+console.log(a, b);
+
+// undefined var when an var is created but not initialed it is given undefined value
+var und = undefined;
+console.log(und);
+var no_value_var;
+console.log(no_value_var);
+
+// null var when a var is created and given null value
+var null_var = null;
+console.log(null_var);
+
+
+// At high level in js we have two kinds of data types 
+/*
+1 . Primitive
+2 . Refrence data types
+*/
