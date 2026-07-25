7522435f212be2ea7974e3a195702a56ee57e806
adixyaxo <aditya0dagar@gmail.com>
2026-07-25 23:05:36 +0530

    type narrowing

---
+++ Typescript/src/typeNarrowing.ts
@@ -0,0 +1,34 @@
+function chai(kind:string | number)
+{
+  if(typeof kind == 'string'){
+    return `Making ${kind} Chai...`
+  }
+  return `Chai order ${kind}`
+}
+
+function serveChai(msg?:string)
+{
+  if(msg){
+    return `Serving ${msg}`
+  }
+  return `Serving Normal Chai`
+}
+
+
+// Exaustive checks
+function orderChai(size:"small"|"medium"|"large"|number)
+{
+  if (size=="small")
+  {
+    return "small chai"
+  }
+  else if (size=="medium")
+  {
+    return "medium chai"
+  }
+  else if (size=="large")
+  {
+    return "large chai"
+  }
+  return `chai order #${size}`
+}
