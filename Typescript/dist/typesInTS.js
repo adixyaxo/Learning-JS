19c1c54bac8a153bd7d1a20c00893341186fe3c9
adixyaxo <aditya0dagar@gmail.com>
2026-07-25 22:46:23 +0530

    types

---
+++ Typescript/dist/typesInTS.js
@@ -0,0 +1,12 @@
+"use strict";
+// Annotation Matlab Mei Samjhaunga
+// Inference matlab apne app samajh jao
+Object.defineProperty(exports, "__esModule", { value: true });
+let drink = "chai";
+// drink = 0 this would give an error
+let cups = Math.random() > 0.5 ? 10 : 5;
+let chaiFlavour = "masala chai";
+chaiFlavour = "Ginger tea";
+let chaiOrder;
+chaiOrder = false;
+//# sourceMappingURL=typesInTS.js.map
---
+++ Typescript/src/typesInTS.ts
@@ -0,0 +1,13 @@
+// Annotation Matlab Mei Samjhaunga
+// Inference matlab apne app samajh jao
+
+let drink = "chai"
+// drink = 0 this would give an error
+let cups = Math.random() > 0.5 ? 10:5
+
+let chaiFlavour:string = "masala chai"
+chaiFlavour = "Ginger tea"
+
+let chaiOrder:boolean
+
+chaiOrder = false
