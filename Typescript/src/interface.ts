7e0eaebfac44bd0dc3254786ae8e71dc14154a0c
Aditya <aditya0dagar@gmail.com>
2026-08-01 18:05:57 +0530

    interface

---
+++ Typescript/src/interface.ts
@@ -0,0 +1,69 @@
+type ChaiOrder = {
+  type:string
+  sugar:number
+  strong:boolean
+}
+
+function makeChai(order:{type:string, sugar:number, strong:number}) {
+  console.log(order)
+}
+
+function serveChai(order:ChaiOrder) {
+  console.log(order)
+}
+
+
+type TeaRecipe = {
+  water:number
+  milk:number
+}
+
+class MasalaChai implements TeaRecipe{
+  water = 100
+  milk = 50
+}
+
+// type CupSize =  "small" | "large" this would give error because
+// A class can only implement an object type or intersection of object types with statically known members.
+
+interface CupSize {
+  size : "small"|"large"
+}
+
+class Chai implements CupSize {
+  size: "small"|"large" = "large"
+}
+
+// type Response = {ok:true} | {ok:false}
+// class myRes implements Response{
+//   ok:boolean = true
+// }
+// A class can only implement an object type or intersection of object types with statically known members.
+
+
+type TeaType = "masala" | "ginger" | "lemon"
+
+function orderChai (t:TeaType)
+{
+  console.log(t)
+}
+
+type BaseChai = {teaLeaves : number}
+type Extra = {masala :number}
+
+type ExtraMasalaChai = BaseChai & Extra
+
+const cup : ExtraMasalaChai = {
+  teaLeaves : 2,
+  masala : 10
+}
+
+type User = {
+  readonly username : string
+  bio?:string
+}
+
+const u1 : User = {username : "Hitesh"}
+// cant do
+// u1.username = "name"
+const u2 : User = {username: "Hitesh", bio:"hitesh.ai"}
